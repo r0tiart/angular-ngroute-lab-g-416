fc49c7c337654bd283867d3e45c9fc59d29bc49d
Todd Motto <todd@toddmotto.com>
2016-03-22 04:35:12 +0000

    chore: Added solution

--- js/app/app.js
+++ js/app/app.js
@@ -1,2 +1,15 @@
 angular
-    .module('app', ['']);
+    .module('app', ['ngRoute'])
+	.config(function ($routeProvider) {
+		console.log('hello');
+		$routeProvider
+			.when('/user/:id', {
+				templateUrl: 'views/user.html',
+				controller: 'UserController as user',
+				resolve: {
+					user: function ($http, $route) {
+						return $http.get('http://0.0.0.0:8882/rest/user/' + $route.current.params.id);
+					}
+				}
+			});
+	});
