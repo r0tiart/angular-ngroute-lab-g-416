7955577fa9cd3de2fee77a7ee3dff425cf40f5c4
Luke Ghenco <lukeghenco@gmail.com>
2016-09-28 17:28:31 -0700

    made routes more readable
    changed syntax of ControllerAs to use `vm` (View Model) instead of `user`

--- js/app/app.js
+++ js/app/app.js
@@ -4,7 +4,7 @@ angular
 		$routeProvider
 			.when('/user/:id', {
 				templateUrl: 'views/user.html',
-				controller: 'UserController as user',
+				controller: 'UserController as vm',
 				resolve: {
 					user: function ($http, $route) {
 						return $http.get('http://0.0.0.0:8882/rest/user/' + $route.current.params.id);
